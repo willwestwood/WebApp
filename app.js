4d53073e7a157676300d70ec703f6d54ed54f6aa
William Westwood <williamwestwood@williams-mbp.home>
2019-10-21 23:46:35 +0100

    Allow token headers

--- app.js
+++ app.js
@@ -17,9 +17,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 //CORS middleware
 var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, x-access-token');
 
   next();
 }
